b98705bb0d4b605fecff67cb3edd9b489da9462c
jhonrivera1100 <jhonrivera416@gmail.com>
2023-08-23 18:01:20 -0500

    primer comit

---
+++ buscador.js
@@ -0,0 +1,46 @@
+
+const resultado=document.querySelectorAll('#resultado');
+const year=document.querySelector("#year");
+const max=new Date().getFullYear();
+const min=max-20;
+
+
+
+document.addEventListener('DOMContentLoaded',()=>{
+
+});
+
+
+
+
+mostrarAutos=()=>{
+array.forEach((au)    
+});
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+const buscar=[{
+materia:"matematicas",
+tema:"algebra",
+calificacion:"5 estrellas",
+profesor:"alberto",
+
+},
+
+
+
+
+
+
+]
